851c0b37f54c8bbdfceb49d706f58089f07c9d64
maksimkedys <maksimkedys@icloud.com>
2024-07-26 00:18:10 +0300

    added infoBox

---
+++ src/components/CourceGoalItem.tsx
@@ -0,0 +1,11 @@
+import { CourceGoalType } from "../App";
+
+interface CourceGoalItemProps {
+  goal: CourceGoalType;
+}
+
+function CourceGoalItem({ goal }: CourceGoalItemProps) {
+  return <div>CourceGoalItem</div>;
+}
+
+export default CourceGoalItem;
--- src/components/CourceGolaList.tsx
+++ src/components/CourceGolaList.tsx
@@ -1,5 +1,7 @@
+import { ReactNode } from "react";
 import { type CourceGoalType } from "../App";
 import CourceGoal from "./CourseGoal";
+import InfoBox from "./InfoBox";
 
 interface CourceGolaListProps {
   goals: CourceGoalType[];
@@ -7,16 +9,28 @@ interface CourceGolaListProps {
 }
 
 function CourceGolaList({ goals, onDeleteGoal }: CourceGolaListProps) {
+  if (goals.length === 0)
+    return (
+      <InfoBox mode="hint">You have no goals yet. Start adding some!</InfoBox>
+    );
+
   return (
-    <ul>
-      {goals.map((goal) => (
-        <li key={goal.id}>
-          <CourceGoal goal={goal} onDeleteGoal={onDeleteGoal}>
-            <p>{goal.desc}</p>
-          </CourceGoal>
-        </li>
-      ))}
-    </ul>
+    <>
+      {goals.length >= 4 && (
+        <InfoBox mode="warning" severity="medium">
+          You collecting a lot of goals. Don't put too much on your plate!
+        </InfoBox>
+      )}
+      <ul>
+        {goals.map((goal) => (
+          <li key={goal.id}>
+            <CourceGoal goal={goal} onDeleteGoal={onDeleteGoal}>
+              <p>{goal.desc}</p>
+            </CourceGoal>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
 
